68559dfdedcab5326fe2baceddee9da8b6929968
Maggie-karuri <karurimargaret111@gmail.com>
2024-03-29 02:14:59 +0300

    Creating and Inserting DOM Nodes

--- index.js
+++ index.js
@@ -9,3 +9,4 @@ newHeader.textContent = 'Your Name is the champion';
 document.body.appendChild(newHeader);
 // Replace export statement with module.exports
 module.exports = { newHeader };
+ul.remove();
